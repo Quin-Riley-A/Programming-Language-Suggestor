39103017736c81d8c5dcb5c1ba238844a29a847d
Quin-Riley-A <quin.asselin1@gmail.com>
2022-08-27 18:57:24 -0400

    establish scoring criteria for answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 function formSelection(){
   let form = document.querySelector("form");
-  form.addEventListener("submit", func_NAME);
+  form.addEventListener("submit", formEval);
 }
 
-function func_NAME(event){
+
+
+function formEval(event){
   event.preventDefault();
   let form = document.querySelector("form");
   const radio1Selection1 = document.querySelector("input[name='radioQuestion1']:checked").value;
@@ -12,7 +14,12 @@ function func_NAME(event){
   const radio1Selection4 = document.querySelector("input[name='radioQuestion4']:checked").value;
   const radio1Selection5 = document.querySelector("input[name='radioQuestion5']:checked").value;
   let selectionArray = [radio1Selection1, radio1Selection2, radio1Selection3, radio1Selection4, radio1Selection5];
-  
+  let scores = [];
+  answerEval(selectionArray[0]);
+  answerEval(selectionArray[1]);
+  answerEval(selectionArray[2]);
+  answerEval(selectionArray[3]);
+  answerEval(selectionArray[4]);
 }
 
 window.addEventListener("load", formSelection)
