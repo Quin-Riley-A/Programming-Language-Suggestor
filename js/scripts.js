e604917f6b226fcf3644a42777338a8b90c27bd7
Quin-Riley-A <quin.asselin1@gmail.com>
2022-08-27 18:47:16 -0400

    collate survey answers to array

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,13 @@ function formSelection(){
 
 function func_NAME(event){
   event.preventDefault();
+  let form = document.querySelector("form");
+  const radio1Selection1 = document.querySelector("input[name='radioQuestion1']:checked").value;
+  const radio1Selection2 = document.querySelector("input[name='radioQuestion2']:checked").value;
+  const radio1Selection3 = document.querySelector("input[name='radioQuestion3']:checked").value;
+  const radio1Selection4 = document.querySelector("input[name='radioQuestion4']:checked").value;
+  const radio1Selection5 = document.querySelector("input[name='radioQuestion5']:checked").value;
+  let selectionArray = [radio1Selection1, radio1Selection2, radio1Selection3, radio1Selection4, radio1Selection5];
   
 }
 
