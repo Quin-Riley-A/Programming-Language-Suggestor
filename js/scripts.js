9ddcdf735092f35ac5a900f87947e404ac842a29
Quin-Riley-A <quin.asselin1@gmail.com>
2022-08-28 02:17:21 -0400

    move score tally to separate function

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,21 @@ function answerEval(answerValue) {
   //languages will be arbitrarily weighted for how intuitive or easy I've found their syntax
 }
 
+//Business Logic
+function scoreTally(startScore, answerArray) {
+  for (let i = 0; i<5; i++){
+    for (let n = 0; n<4; n++){
+      startScore[n] = parseFloat(startScore.at(n)) + parseFloat(answerEval(answerArray.at(i)).at(n));
+      console.log("typeof scores.at(n): " + typeof startScore.at(n) + " space " + parseFloat(startScore.at(n)));
+      console.log("scores n: " + startScore.at(n));
+      console.log(startScore);
+    }
+    console.log("Here is scores: ");
+    console.log(startScore);
+  }
+  return startScore;
+}
+
 //UI Logic
 function formEval(event) {
   event.preventDefault();
@@ -37,25 +52,8 @@ function formEval(event) {
   const radioSelection5 = parseInt((document.querySelector("input[name='radioQuestion5']:checked").value).charAt(8));
   let selectionArray = [radioSelection1, radioSelection2, radioSelection3, radioSelection4, radioSelection5];
   let scores = [0.0, 0.0, 0.0, 0.0];
-  for (let i = 0; i<5; i++){
-    console.log("array for question " + i+1);
-    console.log(answerEval(selectionArray[i])[3]);
-    console.log(typeof answerEval(selectionArray[i])[3]);
-    for (let n = 0; n<4; n++){
-      scores[n] = parseFloat(scores.at(n)) + parseFloat(answerEval(selectionArray.at(i)).at(n));
-      console.log("typeof scores.at(n): " + typeof scores.at(n) + " space " + parseFloat(scores.at(n)));
-      console.log("scores n: " + scores.at(n));
-      console.log(scores);
-    }
-    console.log("Here is scores: ")
-    console.log(scores);
-  }
-  console.log(typeof scores[0]);
-  console.log(scores[0]);
-  answerEval(selectionArray[1]);
-  answerEval(selectionArray[2]);
-  answerEval(selectionArray[3]);
-  answerEval(selectionArray[4]);
+  scores = scoreTally(scores, selectionArray)
+  console.log("final scores: " + scores)
 }
 
 window.addEventListener("load", formSelection)
