c99ab2cf93e6c48a71ed86c6bba43abaa0bbd20e
Quin-Riley-A <quin.asselin1@gmail.com>
2022-08-28 01:58:53 -0400

    parse answers into scoring array w/for loops

--- js/scripts.js
+++ js/scripts.js
@@ -38,18 +38,16 @@ function formEval(event) {
   let selectionArray = [radioSelection1, radioSelection2, radioSelection3, radioSelection4, radioSelection5];
   let scores = [0.0, 0.0, 0.0, 0.0];
   for (let i = 0; i<5; i++){
-    let choice = answerEval(selectionArray[i]) 
-    console.log(choice);
-    console.log("here is parsed int");
-    console.log(answerEval(selectionArray[i])[0]);
-    console.log(typeof answerEval(selectionArray[i])[0]);
+
+    console.log("array for question " + i+1);
+    console.log(answerEval(selectionArray[i])[3]);
+    console.log(typeof answerEval(selectionArray[i])[3]);
     for (let n = 0; n<4; n++){
-      console.log("typeof scores[n]: " + typeof parseFloat(scores[n]) + " space " + parseFloat(scores[n]));
-      scores[n] = scores[n] + parseFloat(answerEval(selectionArray[i])[n])
-      console.log("scores n: " + scores[n])
+      scores[n] = parseFloat(scores.at(n)) + parseFloat(answerEval(selectionArray.at(i)).at(n));
+      console.log("typeof scores.at(n): " + typeof scores.at(n) + " space " + parseFloat(scores.at(n)));
+      console.log("scores n: " + scores.at(n));
+      console.log(scores);
     }
-    console.log(parseFloat(answerEval(selectionArray[i])));
-    scores += parseInt(answerEval(selectionArray[i]));
     console.log("Here is scores: ")
     console.log(scores);
   }
